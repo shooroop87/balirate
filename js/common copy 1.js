6106a07f5aee695d0e9f9789a171b497bb7ff196
Andrey <shooroop87@mail.ru>
2025-10-25 14:26:52 +0300

    v2025-10-25 v2
    v2025-10-25 v2

--- js/common copy 1.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-            $(".first-filter__form button.button--white").click(function () {
-				var curr = $(location).attr('href');
-				window.location.href =curr;
-                //location.curr;
-                
-            });
-        });
